93e405d875d99c31282631516894eab3657b296c
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-07 11:57:47 +0200

    clousures

--- src/main/java/com/example/Operation.java
+++ src/main/java/com/example/Operation.java
@@ -6,11 +6,23 @@ import java.util.function.*;
 
 public class Operation {
     static void run(){
+        Function<Integer, Function<Integer,Integer>> sum = a->b->a+b;
+        
+        //var r = (int a)->(int b)->a+b;
+       
+
         List<BinaryOperator<Integer>> operations = new ArrayList<>();
         operations.add((a,b)->a+b);
         operations.add((a,b)->a*b);
         operations.add((a,b)->a-b);
         operations.add((a,b)->a/b);
         operations.forEach(op->System.out.println(op.apply(2, 2)));
+        var result = sum(5);
+        System.out.println(result.apply(3)); //8
+        result.apply(100); //105
+    }
+    
+    static Function<Integer,Integer> sum(Integer a){
+        return b->a+b;
     }
 }
