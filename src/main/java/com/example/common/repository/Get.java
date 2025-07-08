5527444ad5bcfb9f6919a51df4d1254f9046b286
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-08 13:40:50 +0200

    default method and static metod interface

--- src/main/java/com/example/common/repository/Get.java
+++ src/main/java/com/example/common/repository/Get.java
@@ -1,22 +1,22 @@
 package com.example.common.repository;
 
-
-
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import com.example.common.EntityBase;
 import com.example.common.NotFondException;
 
-public interface Get<T extends EntityBase, ID> extends Data<T>  {
-    default T get(ID id){
+public interface Get<T extends EntityBase, ID> extends Data<T> {
+    default T get(ID id) {
         Stream<T> filter = getData().stream()
-            .filter(e->e.getId().equals(id));        
+                .filter(e -> e.getId().equals(id));
         return unwrap(filter.findFirst());
     }
-    default T unwrap(Optional<T> optional){
-        return optional.orElseThrow(()->{
-            throw new NotFondException("No se encuent el registro");
+
+    static <T extends EntityBase> T unwrap(Optional<T> optional) {
+        return optional.orElseThrow(() -> {
+            throw new NotFondException("No se encuentra el registro");
         });
     }
 }
+
